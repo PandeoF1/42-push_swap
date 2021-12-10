229c92b2475ad220c8581f914949d9d8875d3e26
tnard <theo.nard18@gmail.com>
2021-12-10 13:45:50 +0100

    t

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,25 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 12:58:33 by tnard             #+#    #+#             */
-/*   Updated: 2021/12/09 15:49:46 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2021/12/10 13:14:41 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/push_swap.h"
+
+static int	ft_int_len(int x)
+{
+	int	y;
+
+	y = 0;
+	if (x > 0)
+	{
+		if (x > 9)
+			y += ft_int_len(x / 10);
+		y++;
+	}
+	return (y);
+}
 
 static void	ft_usage(void)
 {
@@ -29,7 +43,7 @@ static int	ft_atoi_check(char *str)
 		i++;
 	while (str[i] != '\0' && ft_isdigit(str[i]))
 		nbr = (nbr * 10) + (str[i++] - '0');
-	if (str[i] != ' ' || str[i] == '\0')
+	if (str[i] != ' ' && str[i] != '\0')
 		return (0);
 	return (1);
 }
@@ -57,7 +71,7 @@ static int	ft_check_one_arg(char *str)
 	x = -1;
 	while (str[++x])
 	{
-		if (!ft_isdigit(str[x]))
+		if (str[x] && str[x] != ' ' && !ft_isdigit(str[x]))
 			return (0);
 		while (str[x] && (ft_atoi_check(str + x)))
 		{
@@ -66,10 +80,10 @@ static int	ft_check_one_arg(char *str)
 				n_double[ft_atoi(str + x)] = 0;
 			else
 				return (0);
-			x++;
+			x += ft_int_len(ft_atoi(str + x));
 			if (str[x] && ft_space_len(str + x) == 1)
 				x++;
-			else if (str[x] && ft_space_len(str + x) == 0 && !str[x + 1])
+			else if (str[x])
 				return (1);
 		}
 	}
